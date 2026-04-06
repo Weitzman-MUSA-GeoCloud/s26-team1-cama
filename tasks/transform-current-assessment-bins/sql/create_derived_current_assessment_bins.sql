9e4c6e7d603e74aef907abb89a22ca9c6d32b89c
mlingye <mlingye@upenn.edu>
2026-04-06 13:21:50 -0400

    Update create_derived_current_assessment_bins.sql

--- tasks/transform-current-assessment-bins/sql/create_derived_current_assessment_bins.sql
+++ tasks/transform-current-assessment-bins/sql/create_derived_current_assessment_bins.sql
@@ -1,10 +1,15 @@
 CREATE OR REPLACE TABLE `derived.current_assessment_bins` AS
 WITH binned AS (
     SELECT
-        CAST(FLOOR(market_value / 10000) * 10000 AS INT64) AS lower_bound,
-        CAST(FLOOR(market_value / 10000) * 10000 + 10000 AS INT64) AS upper_bound
+        CAST(
+            FLOOR(market_value / 10000) * 10000 AS INT64
+        ) AS lower_bound,
+        CAST(
+            FLOOR(market_value / 10000) * 10000 + 10000 AS INT64
+        ) AS upper_bound
     FROM `derived.current_assessments`
-    WHERE market_value IS NOT NULL
+    WHERE
+        market_value IS NOT NULL
         AND market_value >= 0
 )
 
