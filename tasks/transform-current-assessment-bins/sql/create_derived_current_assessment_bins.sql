f5ce8683aca20d894e8eb700d21e6db985815770
mlingye <mlingye@upenn.edu>
2026-04-08 14:01:47 -0400

    Issue 8 (#39)
    * Create create_derived_current_assessments.sql
    
    * Create create_derived_current_assessment_bins.sql
    
    * Create create_derived_current_assessment_bins.sql
    
    * Create main.py
    
    * Create requirements.txt
    
    * Update create_derived_current_assessments.sql
    
    * Update create_derived_current_assessment_bins.sql
    
    * Update create_derived_current_assessment_bins.sql
    
    * Delete tasks/transform-current-assessment-bins/sql/tasks/transform-current-assessment-bins/sql/create_derived_current_assessment_bins.sql

---
+++ tasks/transform-current-assessment-bins/sql/create_derived_current_assessment_bins.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE TABLE `derived.current_assessment_bins` AS
+WITH binned AS (
+    SELECT
+        CAST(
+            FLOOR(
+                market_value / 10000
+            ) * 10000 AS INT64
+        ) AS lower_bound,
+        CAST(
+            FLOOR(
+                market_value / 10000
+            ) * 10000 + 10000 AS INT64
+        ) AS upper_bound
+    FROM `derived.current_assessments`
+    WHERE
+        market_value IS NOT NULL
+        AND market_value >= 0
+)
+
+SELECT
+    lower_bound,
+    upper_bound,
+    COUNT(*) AS property_count
+FROM binned
+GROUP BY
+    lower_bound,
+    upper_bound
+ORDER BY
+    lower_bound;
