1382c28571cfc449f4aaf93aedd757d00f26a4ca
Guangze Sun <sungzzzz01@gmail.com>
2026-04-15 15:54:09 -0400

    Repair Issue #3

--- tasks/pwd-parcels/load-pwd-parcels/sql/core_pwd_parcels.sql
+++ tasks/pwd-parcels/load-pwd-parcels/sql/core_pwd_parcels.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TABLE `__PROJECT_ID__.__BQ_CORE_DATASET__.pwd_parcels` AS
 SELECT
-    tencode AS property_id,
+    LPAD(CAST(brt_id AS STRING), 9, '0') AS property_id,
     *
 FROM `__PROJECT_ID__.__BQ_SOURCE_DATASET__.pwd_parcels`;
