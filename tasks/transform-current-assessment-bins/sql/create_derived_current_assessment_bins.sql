b47d9aebddc44edc5c70e9c7cb162aa90384e0b1
mlingye <mlingye@upenn.edu>
2026-04-22 15:44:11 -0400

    Update create_derived_current_assessment_bins.sql

--- tasks/transform-current-assessment-bins/sql/create_derived_current_assessment_bins.sql
+++ tasks/transform-current-assessment-bins/sql/create_derived_current_assessment_bins.sql
@@ -4,7 +4,8 @@ SELECT
     CAST(FLOOR(predicted_value / 10000) * 10000 + 10000 AS INT64) AS upper_bound,
     COUNT(*) AS property_count
 FROM `{project_id}.{derived_dataset}.current_assessments`
-WHERE predicted_value IS NOT NULL
-  AND predicted_value >= 0
+WHERE
+    predicted_value IS NOT NULL
+    AND predicted_value >= 0
 GROUP BY 1, 2
 ORDER BY 1;
